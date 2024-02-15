1c73abd9e485e7e1300451fe0048a8af2c4d41a0
ram7415 <yram9757@gmail.com>
2024-02-15 15:12:14 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,13 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    romanNUmber = { I =1, V = 5, X= 10, L= 50, C= 100, D =500, M= 1000};
+    romanNUmber = { I =1,
+         V = 5,
+          X= 10,
+           L= 50,
+            C= 100,
+             D =500,
+              M= 1000};
     co 
 };
 
