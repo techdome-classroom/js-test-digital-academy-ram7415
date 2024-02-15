22a890a064c125d5577178a40b4ac9c3d037e49f
ram7415 <yram9757@gmail.com>
2024-02-15 14:50:22 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,13 +2,12 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    if((s===(s)) && (s==={s}) && (s===[s])){
-        console.log(true)
-    }
-    
+var isValid = function (s) {
+  if (s === s && s === { s } && s === [s]) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 };
 
 module.exports = { isValid };
-
-
